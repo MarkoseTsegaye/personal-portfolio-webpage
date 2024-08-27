c93baf02bb94b121fc6cae51f7406bc17b4936b1
Markose Tsegaye <103059891+MarkoseTsegaye@users.noreply.github.com>
2024-08-27 00:52:27 -0400

    Commit #1
    Initial upload of project.

---
+++ Porfolio_App/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+
+  
+# Create your views here.
+def dashboard_view(request):
+    
+    return render(request, "dashboard.html")
+
+def index(request):
+    
+    return render(request, "index.html")
